8ac9a4721f974b3a7f5ea3cddfb35797b37ea03e
stephanieswu <44244579+stephanieswu@users.noreply.github.com>
2019-06-29 21:45:07 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-about = document.querySelector('.aboutBox')
-
-function openAbout() {
-if (about.style.display == "block"){
-  about.style.display = "none"
-} else {
-  about.style.display = "block"
-}
-}
